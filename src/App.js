f0a39c9b7eb9bf32323ebdf5bd4c5022072971d8
kmjlibra <kmjlibra2000@naver.com>
2020-08-11 21:06:25 +0900

    1-1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button, Progress} from ' semantic-ui-react'
+import {Button, Progress} from 'semantic-ui-react'
 function App() {
   return (
     <div>
